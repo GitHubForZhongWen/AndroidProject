c9001c7b4a3e6de79b406b6e3097702527c53148
ZZW <393930276@qq.com>
2016-12-04 16:58:58 +0800

    first commit in AndroidProject

---
+++ coolweather/src/main/java/com/zzw/coolweather/util/Utility.java
@@ -0,0 +1,17 @@
+package com.zzw.coolweather.util;
+
+import com.zzw.coolweather.db.CoolWeatherDB;
+
+/**
+ * Created by ZZW on 2016/12/4.
+ */
+
+public class Utility {
+    /**
+     * 解析和处理服务器返回的数据
+     */
+    public synchronized static boolean handleProvincesResponse(CoolWeatherDB coolWeatherDB, String response) {
+        
+        return true;
+    }
+}
